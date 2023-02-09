ab02ee36dea6e72db984a58097584de0ba3e2eea
billyx86 <billymk777@gmail.com>
2023-02-09 20:51:05 +0000

    Add eraser, grid sizing requirements

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const container = document.querySelector(".grid");
 const modifyGrid = document.getElementById("modifyGrid");
+let chosenColor = "black";
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+function makeRows(gridSize) {
+    container.style.setProperty('--grid-rows', gridSize);
+    container.style.setProperty('--grid-cols', gridSize);
 
-    for (i = 0; i < (rows * cols); i++) {
+    for (i = 0; i < (gridSize * gridSize); i++) {
         let cell = document.createElement("div");
         cell.style.backgroundColor = "white";
         container.insertAdjacentElement("beforeend", cell);
         cell.addEventListener("mouseover", function(){
-            cell.style.backgroundColor = "black";
+            cell.style.backgroundColor = `${chosenColor}`;
         });
     }
 }
 
-makeRows(16, 16);
-
 modifyGrid.addEventListener("click", function() {
-    let rows = prompt("Enter the amount of rows: ");
-    let cols = prompt("Enter the amount of columns: ");
-
-    const elements = document.getElementsByClassName("grid-item");
-    while(elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
+    let gridSize = prompt("Enter the amount of rows: ");
+    if (gridSize > 0 && gridSize <= 150) {
+        const elements = document.getElementsByClassName("grid-item");
+        while(elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
     }
-
-    makeRows(rows, cols);
+        makeRows(gridSize);
+    } else {
+        alert("Input specified must be an integer between 1-150.")
+    }
 })
+
+makeRows(16, 16);
